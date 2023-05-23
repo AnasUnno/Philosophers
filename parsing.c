8c620dac1d1632e37e719e93cea9faac730673a6
Anas Raji Afoua <araji-af@e2r3p4.1337.ma>
2023-05-23 20:47:31 +0100

    mandatory done

--- parsing.c
+++ parsing.c
@@ -87,3 +87,30 @@ void	check_spaces(char **av)
 		i++;
 	}
 }
+
+int	ft_atoi(const char *str)
+{
+	int			sign;
+	long		res;
+	int			i;
+
+	i = 0;
+	sign = 1;
+	res = 0;
+	while (str[i] == ' ' || str[i] == '\t' || str[i] == '\f'
+		||str[i] == '\r' || str[i] == '\n' || str[i] == '\v')
+		i++;
+	if (str[i] == '-')
+	{
+		sign *= -1;
+		i++;
+	}
+	else if (str[i] == '+')
+		i++;
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		res = res * 10 + (str[i] - '0');
+		i++;
+	}
+	return (res * sign);
+}
